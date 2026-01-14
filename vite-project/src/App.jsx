9ad6220aed870c4efb47dc45bac0afed5d0fa851
artur <edutuacal@alu.edu.gva.es>
2026-01-14 13:51:15 +0100

    segundo commit-Práctica 2 - Map y useState

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,12 @@ import Profile from './Components/Profile.jsx'
 import Header from "./Components/Header.jsx";
 import Skill from "./Components/Skill.jsx";
 import Footer from "./Components/Footer.jsx";
+import Skills from "./Components/Skills.jsx"
+import {useState} from "react";
 
 export default function App() {
+    const skills = ["Javascript", "jQuery", "Vue", "Angular", "React", "Node.js"]
+    const [showSkills, setShowSkills] = useState(false);
     return (
         <>
             <Header />
@@ -16,6 +20,11 @@ export default function App() {
                 <Skill text="JavaScript" />
                 <Skill text="Angular" />
             </ul>
+
+            <button onClick={() => setShowSkills(!showSkills)}>
+                click me
+            </button>
+            <Skills array={skills} show={showSkills} />
             <Footer />
         </>
     )
