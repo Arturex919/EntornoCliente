78b35b472450408b1643c350b54fb409f341dc8b
Arturo Calle <edutuacal@alu.edu.gva.es>
2026-01-16 08:56:31 +0100

    tercera practica

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,10 +4,12 @@ import Skill from "./Components/Skill.jsx";
 import Footer from "./Components/Footer.jsx";
 import Skills from "./Components/Skills.jsx"
 import {useState} from "react";
+import Contact from "./Components/Contact.jsx";
 
 export default function App() {
     const skills = ["Javascript", "jQuery", "Vue", "Angular", "React", "Node.js"]
     const [showSkills, setShowSkills] = useState(false);
+    const [showContacto, setShowContacto] = useState(false);
     return (
         <>
             <Header />
@@ -16,15 +18,20 @@ export default function App() {
                 age={23}
                 profession="Desarrollador" />
             <ul>
-
                 <Skill text="JavaScript" />
                 <Skill text="Angular" />
             </ul>
+            <p>Ejercicio 2</p>
 
             <button onClick={() => setShowSkills(!showSkills)}>
                 click me
             </button>
             <Skills array={skills} show={showSkills} />
+
+            <p> Ejercicio 3</p>
+            <button onClick={() => setShowContacto(!showContacto)}>{showContacto?"ocultar":"mostar"}</button>
+            {showContacto && <Contact email="ana@email.com" phone="600 123 456"/>}
+
             <Footer />
         </>
     )
---
+++ vite-project/src/Components/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact({email, phone}){
+    return(
+        <>
+        <h3>Contacto</h3>
+            <p>email={email}</p>
+            <p>phone={phone}</p>
+        </>
+    )
+}
